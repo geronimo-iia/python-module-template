e439fa5bd914bfcfdb64ad05af8d7b8de3861ba7
Jerome Guibert <jguibert@gmail.com>
2025-01-06 13:01:07 +0100

    fix: poetry version usage

---
+++ {{cookiecutter.github_repo}}/tests/test_dummy.py
@@ -0,0 +1,5 @@
+"""Test unit example."""
+
+
+def test_dummy():
+    assert True
--- {{cookiecutter.github_repo}}/tests/test_version.py
+++
@@ -1,7 +0,0 @@
-"""Test unit for module version."""
-
-
-def test_version_is_defined():
-    from  {{cookiecutter.package_name}} import __version__
-
-    assert __version__
